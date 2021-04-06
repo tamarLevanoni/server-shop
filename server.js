b45d4ddf22573bdd0ada2cf129166cd38b08c4ad
Tamar Levanoni <tamari250@gmail.com>
2021-04-06 14:53:13 +0300

    commit

--- server.js
+++ server.js
@@ -4,17 +4,10 @@ const mongoose = require("mongoose");
 const path = require("path");
 require('dotenv').config();
 
-// const fs = require("fs");
-
-// let products = require("./products");
-
-
-
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "client/build")));
-// app.use('/', express.static('client'));
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(express.json());
 app.use(cors());
